5271a6151a93fcbe1149c0102f0a6a59cbb9ade5
sabdiogodana <sabdiyogodanaj@gmail.com>
2022-02-20 23:09:32 +0300

    second trial

---
+++ src/main.kt
@@ -0,0 +1,27 @@
+fun main(){
+    name()
+    modulus(33,5)
+    var result=addition(10,12,20,8)
+    fact()
+}
+
+
+
+fun name(){
+    var name =("Hello sabdio")
+    println(name)
+}
+fun modulus(num1:Int,num2:Int){
+    var modulus= num1%num2
+    println(modulus)
+
+}
+fun addition(a:Int,b:Int,c:Int,d:Int){
+    var add=(a+b+c+d)
+    println(add)
+}
+fun fact(){
+    var fact=("love coding")
+    println(fact)
+
+}
